64077692a4152b8d46ffd208bd6f91c266ab0269
SOLOMONON <solomonodoki@yahoo.com>
2022-12-19 06:06:00 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev -> a function to print a string in reverse
+ * @s: the value to process to the stdout in reverse
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	while (i--)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
